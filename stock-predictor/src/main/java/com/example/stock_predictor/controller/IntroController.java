749274058ce48ff15c356cbbb1fa87c26e708efe
camellia-ver <jakahi435@gmail.com>
2025-08-14 19:50:29 +0900

    (추가)intro 화면 생성

---
+++ stock-predictor/src/main/java/com/example/stock_predictor/controller/IntroController.java
@@ -0,0 +1,12 @@
+package com.example.stock_predictor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IntroController {
+    @GetMapping("/")
+    public String intro(){
+        return "intro";
+    }
+}
