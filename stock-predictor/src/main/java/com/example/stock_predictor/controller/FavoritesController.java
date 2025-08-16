4c5782df63c976ad1544a1121a67a621911ad7fa
camellia-ver <jakahi435@gmail.com>
2025-08-16 14:41:26 +0900

    (추가) favorites 화면 추가

---
+++ stock-predictor/src/main/java/com/example/stock_predictor/controller/FavoritesController.java
@@ -0,0 +1,12 @@
+package com.example.stock_predictor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FavoritesController {
+    @GetMapping("/favorites")
+    public String favorites(){
+        return "favorites";
+    }
+}
