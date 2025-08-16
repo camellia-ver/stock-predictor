8b443c8c99eb2dbd7ad45ea31c3bd5e09c225cf7
camellia-ver <jakahi435@gmail.com>
2025-08-16 15:05:42 +0900

    (수정) spring boot security 임시로 비활성화

---
+++ stock-predictor/src/main/java/com/example/stock_predictor/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.stock_predictor.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+//    public String home(Model model, Authentication authentication){
+//        if (authentication != null && authentication.isAuthenticated()){
+//            model.addAttribute("homeLink","/dashboard");
+//        }else {
+//            model.addAttribute("homeLink","/");
+//        }
+//        return "intro";
+//    }
+    public String intro(){
+        return "intro";
+    }
+}
