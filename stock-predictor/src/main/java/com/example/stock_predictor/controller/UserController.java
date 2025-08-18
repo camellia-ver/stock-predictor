443fb1d25a9a639d0127e632545140325f14b886
camellia-ver <jakahi435@gmail.com>
2025-08-18 15:46:38 +0900

    (추가) user-setting page 추가

--- stock-predictor/src/main/java/com/example/stock_predictor/controller/UserController.java
+++ stock-predictor/src/main/java/com/example/stock_predictor/controller/UserController.java
@@ -14,4 +14,9 @@ public class UserController {
     public String login(){
         return "login";
     }
+
+    @GetMapping("/setting")
+    public String userSettings(){
+        return "user-settings";
+    }
 }
