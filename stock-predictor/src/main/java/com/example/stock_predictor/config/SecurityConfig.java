c08024f7520a04ef89b813390a0398ddc07a150f
camellia-ver <jakahi435@gmail.com>
2025-08-19 15:46:27 +0900

    (추가) spring boot security 설정 작성

--- stock-predictor/src/main/java/com/example/stock_predictor/config/SecurityConfig.java
+++ stock-predictor/src/main/java/com/example/stock_predictor/config/SecurityConfig.java
@@ -3,13 +3,39 @@ package com.example.stock_predictor.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","login","signup").permitAll()
+                        .requestMatchers("/css/**","/js/**","/images/**","favicon.ico").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/",true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+
+        return http.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
