0d307a760e55b73a1bfe552066f474211855dac0
camellia-ver <jakahi435@gmail.com>
2025-08-22 11:00:39 +0900

    (추가) python 설정파일 생성

--- data_save/collect_stock_data.py
+++ data_save/collect_stock_data.py
@@ -6,11 +6,18 @@ from concurrent.futures import ThreadPoolExecutor
 
 def get_korea_stock():
     data = []
-    tickers = stock.get_market_ticker_list(market='ALL')
 
-    for ticker in tickers:
+    kospi_tickers = stock.get_market_ticker_list(market="KOSPI")
+    kosdaq_tickers = stock.get_market_ticker_list(market="KOSDAQ")
+    all_tickers = kospi_tickers + kosdaq_tickers
+
+    for ticker in all_tickers:
         name = stock.get_market_ticker_name(ticker)
         market = "KOSPI" if ticker in stock.get_market_ticker_list("KOSPI") else "KOSDAQ"
         data.append([ticker,name,market,None,datetime.now()])
 
     return pd.DataFrame(data,columns=['ticker','name','market','sector','createAt'])
+
+krx_df = get_korea_stock()
+krx_df.to_csv('korea_stocks.csv',index=False,encoding='utf-8-sig')
+print(f'총 {len(krx_df)}개 종목 csv 저장완료!')
