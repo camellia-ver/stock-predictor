0b8e21696e5c80d0fc08f85d19e3b9b3bf3f3e8b
camellia-ver <jakahi435@gmail.com>
2025-08-22 14:12:24 +0900

    (수정) stock price 수집 코드 함수로 변경

--- data_save/collect_stock_price.py
+++ data_save/collect_stock_price.py
@@ -16,31 +16,35 @@ DB_NAME = os.getenv("DB_NAME")
 db_url = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 engine = create_engine(db_url, echo=False)
 
-tickers_df = pd.read_sql("SELECT ticker FROM stocks", engine)
-tickers = tickers_df['ticker'].tolist()
-
-all_data = []
-
 start_date = '2025-01-01'
 end_date = '2025-08-21'
 
-for ticker in tickers:
-    df = stock.get_market_ohlcv_by_date(start_date,end_date,ticker)
-    df = df.reset_index()
-    df['source'] = 'KRX'
-    df['ticker'] = ticker
+def get_korea_stock_ohlcv(start_date,end_date):
+    tickers_df = pd.read_sql("SELECT ticker FROM stocks", engine)
+    tickers = tickers_df['ticker'].tolist()
+    
+    all_data = []
 
-    df = df.rename(columns={
-        '날짜':'date',
-        '시가':'openPrice',
-        '종가':'closePrice',
-        '고가':'highPrice',
-        '저가':'lowPrice',
-        '거래량':'volume'
-    })
-    df['date'] = pd.to_datetime(df['date']).dt.date
-    all_data.append(df)
+    for ticker in tickers:
+        df = stock.get_market_ohlcv_by_date(start_date,end_date,ticker)
+        if df.empty:
+            continue
+        
+        df = df.reset_index()
+        df['source'] = 'KRX'
+        df['ticker'] = ticker
 
-result_df = pd.concat(all_data, ignore_index=True)
+        df = df.rename(columns={
+            '날짜':'date',
+            '시가':'openPrice',
+            '종가':'closePrice',
+            '고가':'highPrice',
+            '저가':'lowPrice',
+            '거래량':'volume'
+        })
+        df['date'] = pd.to_datetime(df['date']).dt.date
+        all_data.append(df)
 
-print(result_df)    
+    return pd.concat(all_data, ignore_index=True)
+
+print(get_korea_stock_ohlcv(start_date,end_date))    
