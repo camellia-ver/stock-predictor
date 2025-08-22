aa996f71acf02602df6514496534f27cf1eeb00b
camellia-ver <jakahi435@gmail.com>
2025-08-22 10:45:00 +0900

    (수정) 미국 주식 ticker 수집 함수 삭제

--- data_save/collect_stock_data.py
+++ data_save/collect_stock_data.py
@@ -14,12 +14,3 @@ def get_korea_stock():
         data.append([ticker,name,market,None,datetime.now()])
 
     return pd.DataFrame(data,columns=['ticker','name','market','sector','createAt'])
-
-# 1. NASDAQ 전체 티커 목록
-def get_us_tickers():
-    url = "ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqtraded.txt"
-    df = pd.read_csv(url,sep="|")
-    tickers = df[df["Test Issue"] == 'N']['Symbol'].tolist() # 정상종목만
-    return tickers
-
-print(get_us_tickers())
