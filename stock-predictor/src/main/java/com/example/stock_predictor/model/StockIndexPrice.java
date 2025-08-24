20e36aeb6abe2c3447dd1069f785477d8b994550
camellia-ver <jakahi435@gmail.com>
2025-08-24 15:28:26 +0900

    (추가) 지수 테이블 추가

---
+++ stock-predictor/src/main/java/com/example/stock_predictor/model/StockIndexPrice.java
@@ -0,0 +1,35 @@
+package com.example.stock_predictor.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "stock_index_price")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class StockIndexPrice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private BigDecimal openPrice;
+    private BigDecimal closePrice;
+    private BigDecimal highPrice;
+    private BigDecimal lowPrice;
+
+    private Long volume;
+
+    // 거래대금
+    private BigDecimal value;
+
+    @Column(length = 20)
+    private String source;
+}
